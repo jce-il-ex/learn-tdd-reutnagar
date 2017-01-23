79e32f8604fb87077848e3990b1b57e31cd3ea26
reutnagar <salreut1@gmail.com>
2017-01-23 21:47:19 +0200

    Update cash.js

--- cash.js
+++ cash.js
@@ -1,3 +1,4 @@
+//This is a change for the pull request
 var C = {};     // C Object simplifies exporting the module
 C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
